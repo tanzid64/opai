75115da6eed94df3d77d36719a430f01662462b7
Tanzid Haque <tanzid@inbox.ru>
2024-10-25 00:28:12 +0600

    fix: export loading component

--- src/app/auth/callback/loading.tsx
+++ src/app/auth/callback/loading.tsx
@@ -1,10 +1,12 @@
 import { Spinner } from "@/components/global/loader/spinner";
 import { FC } from "react";
 
-export const AuthLoading: FC = () => {
+const AuthLoading: FC = () => {
   return (
     <div className="flex h-screen items-center justify-center w-full">
       <Spinner />
     </div>
   );
 };
+
+export default AuthLoading;
