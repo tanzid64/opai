fd4c6d2523d216c2c4a2948dbc464e5254496616
tanzid64 <tanzid3@gmail.com>
2024-10-30 02:16:19 +0600

    feat: section header component created

---
+++ src/components/global/section-header/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+interface SectionHeaderProps {
+  children?: React.ReactNode;
+  title: string;
+}
+
+export const SectionHeader: FC<SectionHeaderProps> = ({ children, title }) => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        {children}
+        <h2 className="text-[#BDBDBD] text-xl">{title}</h2>
+      </div>
+    </div>
+  );
+};
